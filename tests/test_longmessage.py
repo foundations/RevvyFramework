7561083c20d47bb5e6757b611fae87c4176de640
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-05-26 22:41:34 +0200

    Test that protocol handler validates message type

--- tests/test_longmessage.py
+++ tests/test_longmessage.py
@@ -1,6 +1,6 @@
 import unittest
 
-from revvy.longmessage import LongMessageHandler, LongMessageError
+from revvy.longmessage import LongMessageHandler, LongMessageError, LongMessageType
 
 
 class TestLongMessageHandler(unittest.TestCase):
@@ -9,3 +9,14 @@ class TestLongMessageHandler(unittest.TestCase):
 
         self.assertRaises(LongMessageError, lambda: handler.upload_message([1]))
         self.assertRaises(LongMessageError, handler.finalize_message)
+
+    def test_select_validates_longmessage_type(self):
+        handler = LongMessageHandler(None)
+
+        self.assertRaises(LongMessageError, lambda: handler.select_long_message_type(0))
+        self.assertRaises(LongMessageError, lambda: handler.select_long_message_type(5))
+
+        handler.select_long_message_type(LongMessageType.FIRMWARE_DATA)
+        handler.select_long_message_type(LongMessageType.FRAMEWORK_DATA)
+        handler.select_long_message_type(LongMessageType.CONFIGURATION_DATA)
+        handler.select_long_message_type(LongMessageType.TEST_KIT)
