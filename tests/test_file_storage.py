6ab44312fdd10d9015361342919fbcc87b0ef261
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-05-28 11:43:07 +0200

    Add some file_storage tests

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -1,6 +1,8 @@
+import os
 import unittest
+from mock.mock import patch, mock_open
 
-from revvy.file_storage import MemoryStorage, StorageElementNotFoundError, IntegrityError
+from revvy.file_storage import MemoryStorage, StorageElementNotFoundError, IntegrityError, FileStorage
 
 
 class TestMemoryStorage(unittest.TestCase):
@@ -34,3 +36,50 @@ class TestMemoryStorage(unittest.TestCase):
 
         storage.write('foo', b'data', 'foobar')
         self.assertRaises(IntegrityError, lambda: storage.read('foo'))
+
+
+class TestFileStorage(unittest.TestCase):
+    @patch('revvy.file_storage.open', new_callable=mock_open)
+    def test_folder_access_is_checked_on_init(self, mock):
+        FileStorage('.')  # no error raised
+
+        mock.side_effect = IOError
+        self.assertRaises(IOError, lambda: FileStorage('.'))
+
+    @patch('revvy.file_storage.open', new_callable=mock_open)
+    def test_read_metadata_raises_if_not_found(self, mock):
+        storage = FileStorage('.')
+        mock.reset_mock()
+        mock.side_effect = IOError
+
+        self.assertRaises(StorageElementNotFoundError, lambda: storage.read_metadata('file'))
+
+    @patch('revvy.file_storage.open', new_callable=mock_open)
+    def test_read_raises_if_meta_or_data_file_not_found(self, mock):
+        storage = FileStorage('.')
+        mock.reset_mock()
+        mock.side_effect = [IOError, mock.return_value]
+        self.assertRaises(StorageElementNotFoundError, lambda: storage.read('file'))
+
+        mock.side_effect = [mock.return_value, IOError]
+        self.assertRaises(StorageElementNotFoundError, lambda: storage.read('file'))
+
+    @patch('revvy.file_storage.open', new_callable=mock_open)
+    def test_both_metadata_and_data_is_written(self, mock):
+        storage = FileStorage('.')
+        mock.reset_mock()
+
+        storage.write('file', b'data', 'md5')
+
+        self.assertEqual(2, mock.call_count)
+
+        called_files = list(map(lambda x: x[0][0], mock.call_args_list))
+        expected_files = [
+            os.path.join('.', 'file.meta'),
+            os.path.join('.', 'file.data')
+        ]
+
+        expected_files.sort()
+        called_files.sort()
+
+        self.assertListEqual(expected_files, called_files)
