5e73bd5245c41169bc31f342cf8ed9c460c3eb21
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-06-01 23:11:48 +0200

    Add tests for straight drive direction, python 3.5 doesn't like call objects

--- tests/test_scripting/test_builtin_scripts.py
+++ tests/test_scripting/test_builtin_scripts.py
@@ -1,5 +1,5 @@
 import unittest
-from mock import Mock, call
+from mock import Mock
 
 from revvy.scripting.builtin_scripts import drive_joystick, drive_2sticks
 
@@ -15,7 +15,22 @@ class TestJoystickScripts(unittest.TestCase):
         args = {'robot': robot, 'input': [127, 127]}
         drive_joystick(args)
         self.assertEqual(1, mock.call_count)
-        self.assertEqual(call(0, 0), mock.call_args)
+        self.assertEqual((0, 0), mock.call_args[0])
+
+    def test_vertical_position_is_drive(self):
+        mock = Mock()
+
+        robot = Mock()
+        robot.drivetrain = Mock()
+        robot.drivetrain.set_speeds = mock
+
+        args = {'robot': robot, 'input': [127, 255]}
+        drive_joystick(args)
+        self.assertEqual((-900, 900), mock.call_args[0])
+
+        args = {'robot': robot, 'input': [127, 0]}
+        drive_joystick(args)
+        self.assertEqual((900, -900), mock.call_args[0])
 
 
 class TestStickDriveScripts(unittest.TestCase):
@@ -29,4 +44,19 @@ class TestStickDriveScripts(unittest.TestCase):
         args = {'robot': robot, 'input': [127, 127]}
         drive_2sticks(args)
         self.assertEqual(1, mock.call_count)
-        self.assertEqual(call(0, 0), mock.call_args)
+        self.assertEqual((0, 0), mock.call_args[0])
+
+    def test_right_stick_is_drive(self):
+        mock = Mock()
+
+        robot = Mock()
+        robot.drivetrain = Mock()
+        robot.drivetrain.set_speeds = mock
+
+        args = {'robot': robot, 'input': [127, 255]}
+        drive_joystick(args)
+        self.assertEqual((-900, 900), mock.call_args[0])
+
+        args = {'robot': robot, 'input': [127, 0]}
+        drive_joystick(args)
+        self.assertEqual((900, -900), mock.call_args[0])
