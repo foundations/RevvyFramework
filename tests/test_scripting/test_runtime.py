aa2f29f4e55708010aef0322762776117c7d55db
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-06-03 17:25:25 +0200

    Test callable scripts, test script.stop and manager reset

--- tests/test_scripting/test_runtime.py
+++ tests/test_scripting/test_runtime.py
@@ -31,6 +31,33 @@ mock()''')
 
         self.assertEqual(1, mock.call_count)
 
+    def test_variables_are_passed_to_callable_script_as_args(self):
+        robot_mock = Mock()
+        robot_mock.resources = []
+        robot_mock._motor_ports = []
+        robot_mock._sensor_ports = []
+        robot_mock._remote_controller = Mock()
+        robot_mock._remote_controller.is_button_pressed = Mock(return_value=False)
+
+        mock = Mock()
+
+        sm = ScriptManager(robot_mock)
+        sm.assign('mock', mock)
+        sm.assign('test', self)
+        sm.assign('RobotInterface', RobotInterface)
+
+        def _script(args):
+            args['test'].assertIsInstance(args['robot'], RobotInterface)
+            args['mock']()
+
+        sm.add_script('test', _script)
+
+        sm['test'].start()
+        time.sleep(0.1)
+        sm['test'].cleanup()
+
+        self.assertEqual(1, mock.call_count)
+
     def test_string_script_can_access_variables_assigned_after_creation(self):
         robot_mock = Mock()
         robot_mock.resources = []
@@ -72,8 +99,6 @@ while not ctx.stop_requested:
     pass
 mock()''')
         sm.assign('mock', mock)
-        sm.assign('test', self)
-        sm.assign('RobotInterface', RobotInterface)
 
         # first call, make sure the script runs
         sm['test'].on_stopped(stopped_mock)
@@ -88,8 +113,45 @@ mock()''')
         self.assertEqual(1, stopped_mock.call_count)
 
         # run and check second script
+        sm['test'].on_stopped(stopped_mock)
         sm['test'].start()
         time.sleep(0.1)
-        sm['test'].cleanup()
+
+        # test that stop also stops a script
+        sm['test'].stop()
 
         self.assertEqual(2, mock.call_count)
+        self.assertEqual(2, stopped_mock.call_count)
+
+        sm['test'].cleanup()
+
+    def test_resetting_the_manager_stops_running_scripts(self):
+        robot_mock = Mock()
+        robot_mock.resources = []
+        robot_mock._motor_ports = []
+        robot_mock._sensor_ports = []
+        robot_mock._remote_controller = Mock()
+        robot_mock._remote_controller.is_button_pressed = Mock(return_value=False)
+
+        stopped_mock = Mock()
+
+        sm = ScriptManager(robot_mock)
+        sm.add_script('test', '''
+while not ctx.stop_requested:
+    pass
+''')
+        sm.add_script('test2', '''
+while not ctx.stop_requested:
+    pass
+''')
+
+        # first call, make sure the script runs
+        sm['test'].on_stopped(stopped_mock)
+        sm['test2'].on_stopped(stopped_mock)
+        sm['test'].start()
+        sm['test2'].start()
+        time.sleep(0.1)
+
+        sm.reset()
+
+        self.assertEqual(2, stopped_mock.call_count)
