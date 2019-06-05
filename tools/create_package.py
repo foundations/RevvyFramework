ec6c7d064cd782364d26932933a902303d61b4d3
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-06-05 17:31:40 +0200

    Download required packages and include them in the packaged framework

--- tools/create_package.py
+++ tools/create_package.py
@@ -1,4 +1,5 @@
 import tarfile
+import os
 from os import path
 
 from tools.common import find_files
@@ -7,7 +8,10 @@ from tools.generate_manifest import gen_manifest
 
 
 def create_package(sources, output):
-    print('Creating framework packae: {}'.format(output))
+    print('Downloading requirements')
+    os.popen('pip3 download -r install/requirements.txt -d install/packages').read()
+
+    print('Creating framework package: {}'.format(output))
     prefix = path.join(path.dirname(path.realpath(path.join(__file__, '..'))), '')
 
     with tarfile.open(output, "w:gz") as tar:
@@ -25,5 +29,5 @@ if __name__ == "__main__":
     mf_sources = ['revvy/', 'revvy.py']
     gen_manifest(mf_sources, 'manifest.json')
 
-    package_sources = ['revvy/', 'data/', 'revvy.py', 'manifest.json']
+    package_sources = ['revvy/', 'data/', 'install/requirements.txt', 'install/packages/', 'revvy.py', 'manifest.json']
     create_package(package_sources, 'package.tar.gz')
