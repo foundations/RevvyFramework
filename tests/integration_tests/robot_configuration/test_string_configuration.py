1e24b2a571177d5c37a5cd961978ea82254c3e6f
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-06-24 19:00:25 +0200

    Test that port indexing is consistent between configuration and robot interface

---
+++ tests/integration_tests/robot_configuration/test_string_configuration.py
@@ -0,0 +1,52 @@
+import unittest
+
+from revvy.robot_config import RobotConfig
+from revvy.scripting.robot_interface import PortCollection
+
+
+class TestPortConfiguration(unittest.TestCase):
+    def test_named_ports_can_be_accessed_via_robot_interface(self):
+        """This test case verifies that PortCollection and RobotConfig use the same port numbering conventions"""
+        config_str = '''{
+            "robotConfig": {
+                "motors": [
+                    {
+                        "name": "M1",
+                        "type": 1,
+                        "direction": 0,
+                        "side": 0
+                    },
+                    {
+                        "name": "M2",
+                        "type": 2,
+                        "direction": 1,
+                        "side": 1
+                    }
+                ],
+                "sensors": [
+                    {
+                        "name": "S1",
+                        "type": 1
+                    },
+                    {
+                        "name": "S2",
+                        "type": 2
+                    },
+                    {
+                        "name": "S3",
+                        "type": 0
+                    },
+                    {
+                        "name": "S4",
+                        "type": 1
+                    }
+                ]
+            },
+            "blocklyList": []
+        }'''
+        config = RobotConfig.from_string(config_str)
+        motors = PortCollection([2, 3, 5, 7], config.motors.names)
+        sensors = PortCollection([3, 5, 7, 9], config.sensors.names)
+
+        self.assertEqual(3, motors["M2"])
+        self.assertEqual(9, sensors["S4"])
