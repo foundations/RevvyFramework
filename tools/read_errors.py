74d5abd3c0b06228c62950db91e99197bfe2f32c
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-07-29 16:22:56 +0200

    Always read errors

--- tools/read_errors.py
+++ tools/read_errors.py
@@ -45,48 +45,44 @@ def format_error(error):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--inject-test-error', help='Record an error', action='store_true')
-    parser.add_argument('--read-all', help='Read and display stored errors', action='store_true')
     parser.add_argument('--clear', help='Clear the error memory', action='store_true')
 
     args = parser.parse_args()
 
-    if not (args.read_all or args.clear):
-        parser.print_help()
-    else:
-        with RevvyTransportI2C() as transport:
-            robot_control = RevvyControl(transport.bind(0x2D))
+    with RevvyTransportI2C() as transport:
+        robot_control = RevvyControl(transport.bind(0x2D))
 
-            if args.inject_test_error:
-                print('Recording a test error')
-                robot_control.error_memory_test()
+        if args.inject_test_error:
+            print('Recording a test error')
+            robot_control.error_memory_test()
 
-            if args.read_all:
-                error_count = robot_control.error_memory_read_count()
-                if error_count == 0:
-                    print('There are no errors stored')
-                elif error_count == 1:
-                    print('There is one error stored')
-                else:
-                    print('There are {} errors stored'.format(error_count))
+        # read errors
+        error_count = robot_control.error_memory_read_count()
+        if error_count == 0:
+            print('There are no errors stored')
+        elif error_count == 1:
+            print('There is one error stored')
+        else:
+            print('There are {} errors stored'.format(error_count))
 
-                if error_count > 0:
-                    print('Reading error memory...')
-                    error_list = []
-                    while len(error_list) < error_count:
-                        errors = robot_control.error_memory_read_errors(len(error_list))
-                        if len(errors) == 0:
-                            break
+        if error_count > 0:
+            print('Reading error memory...')
+            error_list = []
+            while len(error_list) < error_count:
+                errors = robot_control.error_memory_read_errors(len(error_list))
+                if len(errors) == 0:
+                    break
 
-                        error_list += errors
+                error_list += errors
 
-                    i = 0
-                    for error in error_list:
-                        print('----------------------------------------')
-                        print('Error {}'.format(i))
-                        print(format_error(error))
-                        i += 1
+            i = 0
+            for error in error_list:
+                print('----------------------------------------')
+                print('Error {}'.format(i))
+                print(format_error(error))
+                i += 1
 
-            if args.clear:
-                print('Clearing error memory...')
-                robot_control.error_memory_clear()
-                print('Error memory cleared')
+        if args.clear:
+            print('Clearing error memory...')
+            robot_control.error_memory_clear()
+            print('Error memory cleared')
