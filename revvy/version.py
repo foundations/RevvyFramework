b6ae74b5bb15b7952c189b28f77be4d2425b45f5
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-07-29 11:17:20 +0200

    Change to standard version format

--- revvy/version.py
+++ revvy/version.py
@@ -1,7 +1,7 @@
 import re
 
 
-version_re = re.compile('(?P<major>\\d+?).(?P<minor>\\d+?)(-r(?P<rev>\\d+))?')
+version_re = re.compile('(?P<major>\\d+?).(?P<minor>\\d+?)(\\.(?P<rev>\\d+))?(-.*?)?$')
 
 
 class FormatError(Exception):
@@ -11,10 +11,10 @@ class FormatError(Exception):
 class Version:
     def __init__(self, ver_str):
         """
-        >>> Version('1.0-r123')
-        Version(1.0-r123)
-        >>> Version('1.0')
-        Version(1.0-r0)
+        >>> Version('1.0.123')
+        Version(1.0.123)
+        >>> Version('1.0-foobar') # optional tag is ignored
+        Version(1.0.0)
         """
         match = version_re.match(ver_str)
         if not match:
@@ -23,7 +23,7 @@ class Version:
         minor = int(match.group('minor'))
         rev = int(match.group('rev')) if match.group('rev') is not None else 0
         self._version = {'major': major, 'minor': minor, 'revision': rev}
-        self._normalized = '{}.{}-r{}'.format(self._version['major'], self._version['minor'], self._version['revision'])
+        self._normalized = '{}.{}.{}'.format(self._version['major'], self._version['minor'], self._version['revision'])
 
     @property
     def major(self):
@@ -44,109 +44,109 @@ class Version:
     @property
     def revision(self):
         """
-        >>> Version('2.3-r45').revision
+        >>> Version('2.3.45').revision
         45
         """
         return self._version['revision']
 
     def __le__(self, other):
         """
-        >>> Version('1.0-r0') <= Version('1.0-r0')
+        >>> Version('1.0.0') <= Version('1.0.0')
         True
-        >>> Version('1.0-r0') <= Version('1.0-r1')
+        >>> Version('1.0.0') <= Version('1.0.1')
         True
-        >>> Version('1.0-r0') <= Version('1.1-r0')
+        >>> Version('1.0.0') <= Version('1.1.0')
         True
-        >>> Version('1.0-r0') <= Version('2.0-r0')
+        >>> Version('1.0.0') <= Version('2.0.0')
         True
-        >>> Version('1.0-r1') <= Version('1.0-r0')
+        >>> Version('1.0.1') <= Version('1.0.0')
         False
-        >>> Version('1.1-r0') <= Version('1.0-r0')
+        >>> Version('1.1.0') <= Version('1.0.0')
         False
-        >>> Version('2.0-r0') <= Version('1.0-r0')
+        >>> Version('2.0.0') <= Version('1.0.0')
         False
         """
         return self.compare(other) != 1
 
     def __eq__(self, other):
         """
-        >>> Version('1.0-r0') == Version('1.0-r0')
+        >>> Version('1.0.0') == Version('1.0.0')
         True
-        >>> Version('1.0-r0') == Version('1.0-r1')
+        >>> Version('1.0.0') == Version('1.0.1')
         False
-        >>> Version('1.0-r0') == Version('1.1-r0')
+        >>> Version('1.0.0') == Version('1.1.0')
         False
-        >>> Version('1.0-r0') == Version('2.0-r0')
+        >>> Version('1.0.0') == Version('2.0.0')
         False
         """
         return self.compare(other) == 0
 
     def __ne__(self, other):
         """
-        >>> Version('1.0-r0') != Version('1.0-r0')
+        >>> Version('1.0.0') != Version('1.0.0')
         False
-        >>> Version('1.0-r0') != Version('1.0-r1')
+        >>> Version('1.0.0') != Version('1.0.1')
         True
-        >>> Version('1.0-r0') != Version('1.1-r0')
+        >>> Version('1.0.0') != Version('1.1.0')
         True
-        >>> Version('1.0-r0') != Version('2.0-r0')
+        >>> Version('1.0.0') != Version('2.0.0')
         True
         """
         return self.compare(other) != 0
 
     def __lt__(self, other):
         """
-        >>> Version('1.0-r0') < Version('1.0-r0')
+        >>> Version('1.0.0') < Version('1.0.0')
         False
-        >>> Version('1.0-r0') < Version('1.0-r1')
+        >>> Version('1.0.0') < Version('1.0.1')
         True
-        >>> Version('1.0-r0') < Version('1.1-r0')
+        >>> Version('1.0.0') < Version('1.1.0')
         True
-        >>> Version('1.0-r0') < Version('2.0-r0')
+        >>> Version('1.0.0') < Version('2.0.0')
         True
-        >>> Version('1.0-r1') < Version('1.0-r0')
+        >>> Version('1.0.1') < Version('1.0.0')
         False
-        >>> Version('1.1-r0') < Version('1.0-r0')
+        >>> Version('1.1.0') < Version('1.0.0')
         False
-        >>> Version('2.0-r0') < Version('1.0-r0')
+        >>> Version('2.0.0') < Version('1.0.0')
         False
         """
         return self.compare(other) == -1
 
     def __gt__(self, other):
         """
-        >>> Version('1.0-r0') > Version('1.0-r0')
+        >>> Version('1.0.0') > Version('1.0.0')
         False
-        >>> Version('1.0-r0') > Version('1.0-r1')
+        >>> Version('1.0.0') > Version('1.0.1')
         False
-        >>> Version('1.0-r0') > Version('1.1-r0')
+        >>> Version('1.0.0') > Version('1.1.0')
         False
-        >>> Version('1.0-r0') > Version('2.0-r0')
+        >>> Version('1.0.0') > Version('2.0.0')
         False
-        >>> Version('1.0-r1') > Version('1.0-r0')
+        >>> Version('1.0.1') > Version('1.0.0')
         True
-        >>> Version('1.1-r0') > Version('1.0-r0')
+        >>> Version('1.1.0') > Version('1.0.0')
         True
-        >>> Version('2.0-r0') > Version('1.0-r0')
+        >>> Version('2.0.0') > Version('1.0.0')
         True
         """
         return self.compare(other) == 1
 
     def __ge__(self, other):
         """
-        >>> Version('1.0-r0') >= Version('1.0-r0')
+        >>> Version('1.0.0') >= Version('1.0.0')
         True
-        >>> Version('1.0-r0') >= Version('1.0-r1')
+        >>> Version('1.0.0') >= Version('1.0.1')
         False
-        >>> Version('1.0-r0') >= Version('1.1-r0')
+        >>> Version('1.0.0') >= Version('1.1.0')
         False
-        >>> Version('1.0-r0') >= Version('2.0-r0')
+        >>> Version('1.0.0') >= Version('2.0.0')
         False
-        >>> Version('1.0-r1') >= Version('1.0-r0')
+        >>> Version('1.0.1') >= Version('1.0.0')
         True
-        >>> Version('1.1-r0') >= Version('1.0-r0')
+        >>> Version('1.1.0') >= Version('1.0.0')
         True
-        >>> Version('2.0-r0') >= Version('1.0-r0')
+        >>> Version('2.0.0') >= Version('1.0.0')
         True
         """
         return self.compare(other) != -1
@@ -154,11 +154,11 @@ class Version:
     # noinspection PyProtectedMember
     def compare(self, other):
         """
-        >>> Version('1.0-r0').compare(Version('1.0-r0'))
+        >>> Version('1.0.0').compare(Version('1.0.0'))
         0
-        >>> Version('1.0-r1').compare(Version('1.0-r0'))
+        >>> Version('1.0.1').compare(Version('1.0.0'))
         1
-        >>> Version('1.0-r0').compare(Version('1.0-r1'))
+        >>> Version('1.0.0').compare(Version('1.0.1'))
         -1
         """
         if self._version['major'] == other._version['major']:
@@ -175,7 +175,7 @@ class Version:
     def __str__(self) -> str:
         """
         >>> str(Version('1.0'))
-        '1.0-r0'
+        '1.0.0'
         """
         return self._normalized
 
