3cc4ef742e687ab8d56d6276c1bd0307c79c399d
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-08-08 17:18:42 +0200

    Trigger image build if tagged

--- revvy/fw_version.py
+++ revvy/fw_version.py
@@ -1,2 +1,2 @@
 # This file is generated before each commit
-FRAMEWORK_VERSION = "0.1.598"
+FRAMEWORK_VERSION = "0.1.599"
---
+++ tools/trigger-build.sh
@@ -0,0 +1,13 @@
+body='{
+"message": "Build triggered by tag $TRAVIS_TAG on RevvyFramework",
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $TRAVIS_API_KEY" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/RevolutionRobotics%2FBakeRPi/requests
