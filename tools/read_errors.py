c629b45c0ea0e5884d5cfeaac212b27bd8edbdda
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-08-09 20:54:33 +0200

    Cleanup

--- tools/read_errors.py
+++ tools/read_errors.py
@@ -1,8 +1,67 @@
 import argparse
+import enum
 import traceback
 
 from revvy.hardware_dependent.rrrc_transport_i2c import RevvyTransportI2C
 from revvy.mcu.rrrc_control import RevvyControl
+
+
+class ErrorType(enum.IntEnum):
+    HardFault = 0
+    StackOverflow = 1
+    AssertFailure = 2
+    TestError = 3
+
+
+hw_formats = {
+    0: '1.0.0',
+    1: '1.0.1',
+    2: '2.0.0'
+}
+
+fw_formats = {
+    0: '0.1.{}',
+    1: '0.1.{}',
+    2: '0.2.{}'
+}
+
+
+exception_names = [
+    'Hard fault',
+    'Stack overflow',
+    'Assertion failure',
+    'Test error'
+]
+
+
+cfsr_reasons = [
+    "The processor has attempted to execute an undefined instruction",
+    "The processor attempted a load or store at a location that does not permit the operation",
+    None,
+    "Unstack for an exception return has caused one or more access violations",
+    "Stacking for an exception entry has caused one or more access violations",
+    "A MemManage fault occurred during floating-point lazy state preservation",
+    None,
+    None,
+    "Instruction bus error",
+    "Data bus error (PC value points to the instruction that caused the fault)",
+    "Data bus error (PC value is not directly related to the instruction that caused the error)",
+    "Unstack for an exception return has caused one or more BusFaults",
+    "Stacking for an exception entry has caused one or more BusFaults",
+    "A bus fault occurred during floating-point lazy state preservation",
+    None,
+    None,
+    "The processor has attempted to execute an undefined instruction",
+    "The processor has attempted to execute an instruction that makes illegal use of the EPSR",
+    "The processor has attempted an illegal load to the PC",
+    "The processor has attempted to access a coprocessor",
+    None,
+    None,
+    None,
+    None,
+    "The processor has made an unaligned memory access",
+    "The processor has executed an SDIV or UDIV instruction with a divisor of 0",
+]
 
 
 def format_error(error):
@@ -13,7 +72,7 @@ def format_error(error):
         fw_version = error[5:9]
         error_data = error[9:]
 
-        if error_id == 0:
+        if error_id == ErrorType.HardFault:
             pc = int.from_bytes(error_data[0:4], byteorder='little')
             psr = int.from_bytes(error_data[4:8], byteorder='little')
             lr = int.from_bytes(error_data[8:12], byteorder='little')
@@ -21,38 +80,8 @@ def format_error(error):
             dfsr = int.from_bytes(error_data[16:20], byteorder='little')
             hfsr = int.from_bytes(error_data[20:24], byteorder='little')
 
-            exception_name = 'Hard fault'
             details_str = '\n\tPC: 0x{0:X}\tPSR: 0x{1:X}\tLR: 0x{2:X}'.format(pc, psr, lr)
             details_str += '\n\tCFSR: 0x{0:X}\tDFSR: 0x{1:X}\tHFSR: 0x{2:X}'.format(cfsr, dfsr, hfsr)
-
-            cfsr_reasons = [
-                "The processor has attempted to execute an undefined instruction",
-                "The processor attempted a load or store at a location that does not permit the operation",
-                None,
-                "Unstack for an exception return has caused one or more access violations",
-                "Stacking for an exception entry has caused one or more access violations",
-                "A MemManage fault occurred during floating-point lazy state preservation",
-                None,
-                None,
-                "Instruction bus error",
-                "Data bus error (PC value points to the instruction that caused the fault)",
-                "Data bus error (PC value is not directly related to the instruction that caused the error)",
-                "Unstack for an exception return has caused one or more BusFaults",
-                "Stacking for an exception entry has caused one or more BusFaults",
-                "A bus fault occurred during floating-point lazy state preservation",
-                None,
-                None,
-                "The processor has attempted to execute an undefined instruction",
-                "The processor has attempted to execute an instruction that makes illegal use of the EPSR",
-                "The processor has attempted an illegal load to the PC",
-                "The processor has attempted to access a coprocessor",
-                None,
-                None,
-                None,
-                None,
-                "The processor has made an unaligned memory access",
-                "The processor has executed an SDIV or UDIV instruction with a divisor of 0",
-            ]
 
             if cfsr != 0:
                 details_str += "\n\tReasons:"
@@ -60,46 +89,34 @@ def format_error(error):
                     if (cfsr & 1 << bit) != 0 and cfsr_reasons[bit] is not None:
                         details_str += "\n\t\t" + cfsr_reasons[bit]
 
-        elif error_id == 1:
+        elif error_id == ErrorType.StackOverflow:
             task = bytes(error_data).decode("utf-8")
-
-            exception_name = 'Stack overflow'
             details_str = '\nTask: {}'.format(task)
 
-        elif error_id == 2:
+        elif error_id == ErrorType.AssertFailure:
             line = int.from_bytes(error_data[0:4], byteorder='little')
             file = bytes(error_data[4:]).decode("utf-8")
-
-            exception_name = 'Assertion failure'
             details_str = '\nFile: {}, Line: {}'.format(file, line)
 
-        elif error_id == 3:
-            exception_name = 'Test error'
+        elif error_id == ErrorType.TestError:
             details_str = '\nData: {}'.format(error_data)
 
         else:
-            exception_name = 'Unknown error'
             details_str = '\nData: {}'.format(error_data)
 
         hw = int.from_bytes(hw_version, byteorder='little')
         fw = int.from_bytes(fw_version, byteorder='little')
 
-        hw_formats = {
-            0: '1.0.0',
-            1: '1.0.1',
-            2: '2.0.0'
-        }
-
-        fw_formats = {
-            0: '0.1.{}',
-            1: '0.1.{}',
-            2: '0.2.{}'
-        }
-
         hw_str = hw_formats[hw]
         fw_str = fw_formats[hw].format(fw)
 
+        try:
+            exception_name = exception_names[error_id]
+        except IndexError:
+            exception_name = 'Unknown error'
+
         return '{} ({}, HW: {}, FW: {})\nDetails: {}'.format(exception_name, error_id, hw_str, fw_str, details_str)
+
     except Exception:
         traceback.print_exc()
         return 'Error during processing\nRaw data: {}'.format(error)
@@ -128,21 +145,19 @@ if __name__ == "__main__":
         else:
             print('There are {} errors stored'.format(error_count))
 
-        if error_count > 0:
-            print('Reading error memory...')
-            error_list = []
-            while len(error_list) < error_count:
-                errors = robot_control.error_memory_read_errors(len(error_list))
-                if len(errors) == 0:
-                    break
+        remaining = error_count
+        i = 0
+        while remaining > 0:
+            errors = robot_control.error_memory_read_errors(remaining)
+            if len(errors) == 0:
+                break
 
-                error_list += errors
+            remaining -= len(errors)
 
-            i = 0
-            for error in error_list:
+            for err in errors:
                 print('----------------------------------------')
                 print('Error {}'.format(i))
-                print(format_error(error))
+                print(format_error(err))
                 i += 1
 
         if args.clear:
