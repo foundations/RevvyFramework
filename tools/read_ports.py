abaa429dc1c3c0527c24caafb9cf5c77c0521ef8
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-10-07 11:56:42 +0200

    Read IMU yaw angle

--- tools/read_ports.py
+++ tools/read_ports.py
@@ -21,10 +21,11 @@ if __name__ == "__main__":
     parser.add_argument('--s2', help='Configure S2', default=None, choices=port_choices)
     parser.add_argument('--s3', help='Configure S3', default=None, choices=port_choices)
     parser.add_argument('--s4', help='Configure S4', default=None, choices=port_choices)
+    parser.add_argument('--imu-angle', help='Read IMU yaw angle', action='store_true')
 
     args = parser.parse_args()
 
-    if not (args.s1 or args.s2 or args.s3 or args.s4):
+    if not (args.s1 or args.s2 or args.s3 or args.s4 or args.imu_angle):
         print('No ports configured')
         sys.exit(0)
 
@@ -37,9 +38,11 @@ if __name__ == "__main__":
         pattern += "\t{3}"
     if args.s4:
         pattern += "\t{4}"
+    if args.imu_angle:
+        pattern += "\t{5}"
 
     sensor_data_changed = False
-    sensor_data = [0, None, None, None, None]
+    sensor_data = [0, None, None, None, None, None]
 
     with RevvyTransportI2C() as transport:
         robot_control = RevvyControl(transport.bind(0x2D))
@@ -49,6 +52,13 @@ if __name__ == "__main__":
             global sensor_data_changed
             sensor_data_changed = False
             robot.update_status()
+
+            if args.imu_angle:
+                angle = robot.imu.yaw_angle
+                if angle != sensor_data[5]:
+                    sensor_data[5] = angle
+                    sensor_data_changed = True
+
             if sensor_data_changed:
                 sensor_data[0] = round(time.time() - robot.start_time, 2)
                 print(pattern.format(*sensor_data))
@@ -64,6 +74,7 @@ if __name__ == "__main__":
             sensor.configure(port_config_map[name])
             sensor.on_value_changed(lambda p: sensor_value_changed(index, p.value))
 
+        robot.reset()
         status_update_thread = periodic(update, 0.02, "RobotStatusUpdaterThread")
         status_update_thread.start()
 
